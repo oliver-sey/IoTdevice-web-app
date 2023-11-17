9626f0732fce661384b82198699fa451e7a46e6c
Oliver Seymour <oliver@seymourfam.com>
2023-11-17 16:30:36 -0700

    Got adding the click handlers working

--- public/javascript/signin.js
+++ public/javascript/signin.js
@@ -6,11 +6,22 @@ let requestBody = {
 
 $(document).ready(()=>{
     console.log("hostname", window.location.hostname)
+    // call the function that will add all the click handlers
+    // we put this in a function because it didn't seem to add the handlers
+    // if we did it outside a function
+    console.log("Document is ready, calling addClickHandlers()")
+    addClickHandlers()
 })
 
-$("#signin").click(() => {
-    console.log("hostname", window.location.pathname)
-})
+
+// **** put all your adding click handlers in here!!!
+// this gets called when the document is ready
+function addClickHandlers() {
+    console.log("in the addClickHandlers function")
+    
+    // when you click 'Sign in' button on the login screen, call logIn()
+    $("#signin").click(logIn)
+}
 
 
 function setRequestBody() {
@@ -19,6 +30,7 @@ function setRequestBody() {
 }
 
 function logIn() {
+    console.log("In the logIn() function")
     //setRequestBody()
     console.log("hostname", window.location.pathname)
     // $.ajax({
