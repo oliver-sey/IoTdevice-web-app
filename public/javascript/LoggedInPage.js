052201f70cd20f9c3cb03b2217399663d63c7ee1
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-11-25 12:29:48 -0700

    add devices page

--- public/javascript/LoggedInPage.js
+++ public/javascript/LoggedInPage.js
@@ -15,13 +15,14 @@ $(document).ready(()=>{
         console.log("my devices", res, res.length)
         //ex https://api.thingspeak.com/channels/2349152/charts/1?api_key=MPQACWXEJVYHLC7K
         for (let i = 0; i < res.length; i++) {
-            $('#deviceStart').append(`<p id="${i}">Device ${i + 1} ${res[i].deviceName}<a href="https://api.thingspeak.com/channels/${res[i].channelID}/charts/1?api_key=${res[i].readAPI_Key}">View chart</a></p>`)
+            $('#deviceStart').append(`<p id="${i}">Device ${i + 1} ${res[i].deviceName}<a href="./device.html?deviceName=${res[i].deviceName}">View this device</a></p>`)
         }
     })
      .fail(function(err) {
         $('#deviceStart').text("You have no device yet!")
         console.log("my devices err", err)
      })
+     
     console.log("Document is ready, calling addClickHandlers()")
     addClickHandlers()
 })
@@ -44,10 +45,17 @@ function addClickHandlers() {
      .fail(function(err) {
         alert("Login Fail")
      });
+
+     $('#logOut').click(logOut)
 }
 
 function setRequestBody() {
     requestBody.email = localStorage.getItem('email')
     requestBody.password = localStorage.getItem('password')
     console.log("requestBody", requestBody)
+}
+
+function logOut() {//after logout, clear local storage
+    localStorage.clear()
+    alert("You have successfully log out!")
 }
---
+++ public/javascript/device.js
@@ -0,0 +1,15 @@
+
+$(document).ready(()=>{
+    console.log("hostname", window.location.hostname)
+    $.ajax({
+        url: `/device/mydevices?deviceName=${urlParams.get('deviceName')}`,
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json"
+    })
+    .done(function (res) {
+        console.log("device res", res)
+    })
+})
+
+//<a href="https://api.thingspeak.com/channels/${res[i].channelID}/charts/1?api_key=${res[i].readAPI_Key}">View this device</a>
