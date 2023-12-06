3744e3799a343229bf195ada7e83596eee548dcd
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-12-06 15:48:00 -0700

    add both graph

--- public/javascript/weeklysummary.js
+++ public/javascript/weeklysummary.js
@@ -16,7 +16,7 @@ $(document).ready(()=>{
         console.log("my devices", res, res.length)
         //ex https://api.thingspeak.com/channels/2349152/charts/1?api_key=MPQACWXEJVYHLC7K
         for (let i = 0; i < res.length; i++) {
-            $('h1').after(`<div><p>Device ${i+1}: ${res[i].deviceName}</p><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://api.thingspeak.com/channels/${res[i].channelID}/charts/1?api_key=${res[i].readAPI_Key}&title=Heart_Rate&days=7&yaxis=Heart_Rate(BPM)&results=1000"></div>`)
+            $(`#div${i}`).after(`<div id="div${i+1}"><p>Device ${i+1}: ${res[i].deviceName}</p><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://api.thingspeak.com/channels/${res[i].channelID}/charts/1?api_key=${res[i].readAPI_Key}&title=Heart_Rate&days=7&yaxis=Heart_Rate(BPM)&results=1000"></iframe> <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://api.thingspeak.com/channels/${res[i].channelID}/charts/2?api_key=${res[i].readAPI_Key}&title=Blood_Oxygen_Saturation&days=7&yaxis=Blood_Oxygen_Saturation(BOS)&results=1000"></iframe></div>`)
             //<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/250296/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=50&type=line&update=15">
         }
     })
